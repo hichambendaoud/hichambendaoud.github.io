cba8df7f01b43155cbd5dcdd6edb34b5c7365d8d
Hicham Ben Daoud <hichambendaoudofficiel@gmail.com>
2025-12-07 17:22:49 +0100

    Switch from BrowserRouter to HashRouter

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ import { Toaster } from "@/components/ui/toaster";
 import { Toaster as Sonner } from "@/components/ui/sonner";
 import { TooltipProvider } from "@/components/ui/tooltip";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { HashRouter, Routes, Route } from "react-router-dom"; // Changed from BrowserRouter to HashRouter
 import Index from "./pages/Index";
 import ProjectsList from "./pages/ProjectsList";
 import ProjectDetail from "./pages/ProjectDetail";
@@ -17,7 +17,8 @@ const App = () => (
     <TooltipProvider>
       <Toaster />
       <Sonner />
-      <BrowserRouter>
+      {/* HashRouter handles routing safely on GitHub Pages */}
+      <HashRouter>
         <Routes>
           <Route path="/" element={<Index />} />
           <Route path="/projects" element={<ProjectsList />} />
@@ -27,7 +28,7 @@ const App = () => (
           {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
           <Route path="*" element={<NotFound />} />
         </Routes>
-      </BrowserRouter>
+      </HashRouter>
     </TooltipProvider>
   </QueryClientProvider>
 );
